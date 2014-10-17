0618eb9e5d99a9438e58738b3b6f3b6d7c534b43
Kun Lin <lina@cua.edu>
2014-10-17 08:42:13 -0400

    Summon2.js Summon Config
    Summon Config

---
+++ Summon2.js
@@ -0,0 +1,347 @@
+
+var __configData = {
+    "toolbar": {
+        "add_results_beyond_catalog": true,
+        "exclude_newspaper_articles": true,
+        "full_text_online": true,
+        "items_at_my_institution": false,
+        "items_in_catalog": true,
+        "limit_to_peer_reviewed": false,
+        "limit_to_scholarly": true
+    },
+    "web_of_science": {
+        "metadata_flyout_enabled": true,
+        "metadata_url": ""
+    },
+    "header": {
+        "enabled": false,
+        "tabs": []
+    },
+    "style": {
+        "enable_custom_colors": false,
+        "primary_link_color": "#0088cc",
+        "primary_link_visited_color": "#0088cc",
+        "primary_link_hover_color": "#005580",
+        "secondary_link_color": "#333333",
+        "secondary_link_visited_color": "#333333",
+        "secondary_link_hover_color": "#333333",
+        "selected_facet_link_color": "#f58c31",
+        "selected_facet_link_hover_color": "#f58c31",
+        "auth_banner_background_color": "#505050",
+        "auth_banner_link_color": "#e6e6e6",
+        "auth_banner_link_visited_color": "#e6e6e6",
+        "auth_banner_link_hover_color": "#e6e6e6",
+        "header_background_color": "#e3e3e3",
+        "header_link_color": "#0088cc",
+        "header_link_visited_color": "#005580",
+        "header_link_hover_color": "#666666",
+        "custom_javascript": "http://www.lib.cua.edu/common/summon/Summon2.js"
+    },
+    "widgets": {
+        "chat_widget_email": "",
+        "chat_widget_location": "right",
+        "chat_widget_offline_image": "http://assets.summon.serialssolutions.com/50b77048a61f866b89000c32",
+        "chat_widget_online_image": "http://assets.summon.serialssolutions.com/50b76e40a61f866b89000c30",
+        "enable_autocomplete": true,
+        "enable_libraryh3lp_chat": true,
+        "enable_springshare_chat": false,
+        "enable_syndeticsplus": false,
+        "libraryh3lp_queue": "cuaim",
+        "springshare_domain": "",
+        "springshare_iid": "",
+        "springshare_key": "",
+        "springshare_uid": "",
+        "syndeticsplus_id": "",
+        "enable_widgets_link": true
+    },
+    "metrics": {
+        "enabled": false,
+        "url": "/2.0.0/event"
+    },
+    "worldcat": {
+        "api_key": "",
+        "base_url": "http://www.worldcat.org",
+        "enabled": false,
+        "exclude_oclc_codes": "",
+        "show_all_records_url": "http://www.worldcat.org/search?q="
+    },
+    "flow": {
+        "enabled": true,
+        "auth_url": "https://flow.proquest.com/auth/login/",
+        "signup_url": "https://flow.proquest.com/auth/signup/",
+        "logout_url": "https://flow.proquest.com/auth/logout/",
+        "library_url": "https://flow.proquest.com/auth/library/",
+        "client_id": "summon"
+    },
+    "advanced_search": {
+        "direct_date_entry": true,
+        "enabled": true,
+        "exclude_from_results": true,
+        "expand_results": true,
+        "items_in_library_catalog": true,
+        "items_with_full_text": true,
+        "peer_reviewed": false,
+        "scholarly_materials": true,
+        "show_only": true
+    },
+    "detail_page": {
+        "enable_a_i": true,
+        "enable_ill": true,
+        "enable_opac": true,
+        "enable_reserve_links": true,
+        "ill_base_url": "http://ag7fp7va8v.search.serialssolutions.com",
+        "messages": [{
+            "header": "",
+            "snippet": "<p>&nbsp;</p><script src=\"http://www.lib.cua.edu/common/summon/available.js\"></script>"
+        }],
+        "reserve_links": {
+            "W9Z": "https://www.aladin.wrlc.org/Z-WEB/CLSReqForm?srcid=voyager:WRLC&bibid={{id}}",
+			"Z6W": "https://www.aladin.wrlc.org/Z-WEB/CLSReqForm?srcid=voyager:WRLC&bibid={{id}}&dbid=Z6W",
+			"-G0": "https://www.aladin.wrlc.org/Z-WEB/CLSReqForm?srcid=voyager:WRLC&bibid={{id}}&dbid=-G0"
+        },
+        "sections": ["custom_messages", "toc_syndetics", "summary_syndetics", "reviews_syndetics", "reviews"],
+        "a_i_primary_action": "ill",
+        "enable_primary_button": true,
+        "enable_secondary_button": true
+    },
+    "search_results": {
+        "display_citation_counts_enabled": true
+    },
+    "maverick": {
+        "librarian_contact_url": "http://libraries.cua.edu/about/ask.cfm"
+    },
+    "site": {
+        "about": "http://guides.lib.cua.edu/searchbox",
+        "always_authd": false,
+        "authorized_ip": ["136.242.0.0/16", "192.245.136.3/32"],
+        "custom_link": "http://libraries.cua.edu/res/chat.html",
+        "custom_link_image": "",
+        "custom_link_label": "<img src=\"http://www.lib.cua.edu/chat/status.php\" />Ask a Librarian",
+        "db_recommender_enabled": false,
+        "default_language": "en",
+        "default_search_result_count": 25,
+        "dock_text": "",
+        "email": "searchbox@mail.lib.cua.edu",
+        "enable_one_click_ie": true,
+        "enable_query_suggestions": true,
+        "enable_related_records": true,
+        "enable_spotlighting": true,
+        "enable_transliterations": true,
+        "enable_vpn_banner": true,
+        "favicon_url": "http://assets.summon.serialssolutions.com/4fecbc92a61f866b89000820",
+        "feedback_href": "http://www.lib.cua.edu/wordpress/suggestions/submit/",
+        "ga_tracking_code": "UA-1836085-11",
+        "help": "",
+        "home": "http://libraries.cua.edu",
+        "languages": [{
+            "name": "English",
+            "locale": "en"
+        }],
+        "large_logo_url": "http://assets.summon.serialssolutions.com/5036901ce8c1955a52000a68",
+        "link_resolver": "http://AG7FP7VA8V.search.serialssolutions.com",
+        "private": false,
+        "proxy_catalog_links": false,
+        "proxy_refworks_exports": true,
+        "never_proxy_refworks_exports": false,
+        "proxy_url": "",
+        "query_suggestion_count": 8,
+        "refworks_export_url": "http://www.refworks.com/express/ExpressImport.asp",
+        "saved_items_help_link": "",
+        "show_availability_callnums": true,
+        "show_availability_deweys": false,
+        "show_callnums": false,
+        "show_deweys": false,
+        "show_preview_callnums": true,
+        "show_preview_deweys": false,
+        "small_logo_url": "http://assets.summon.serialssolutions.com/50369000e8c1955a52000a66",
+        "splash_page_url": "http://www.google.com",
+        "spotlight_position": 3,
+        "spotlighting_params": "",
+        "title": "Search Box",
+        "user_survey": false,
+        "user_survey_code": "",
+        "vpn_banner": "Off-campus? Please click here to login and access more resources. ",
+        "vpn_banner_url": "",
+        "enable_maverick": false,
+        "enable_language_detection": false
+    },
+    "citation_formats": {
+        "formats": [{
+            "name": "apa",
+            "label": "APA",
+            "description": "American Psychological Assoc."
+        }, {
+            "name": "ama",
+            "label": "AMA",
+            "description": "American Medical Assoc."
+        }, {
+            "name": "mla",
+            "label": "MLA",
+            "description": "Modern Language Association"
+        }, {
+            "name": "uniform",
+            "label": "Uniform"
+        }, {
+            "name": "chicago_turabian_author_date",
+            "label": "Chicago/Turabian: Author-Date"
+        }, {
+            "name": "harvard",
+            "label": "Harvard"
+        }]
+    },
+    "facets": {
+        "conditional_facets": ["Author", "Genre", "GeographicLocations", "TemporalSubjectTerms", "Discipline", "Library"],
+        "conditional_facets2": ["Author", "Genre", "GeographicLocations", "TemporalSubjectTerms", "Library"],
+        "conditional_union_catalog_facets_enabled": true,
+        "enable_conditional_facets": true,
+        "facets": [{
+            "Name": "ContentType",
+            "Count": "6"
+        }, {
+            "Name": "Institution",
+            "Count": "6"
+        }, {
+            "Name": "Library",
+            "Count": "6"
+        }, {
+            "Name": "Discipline"
+        }, {
+            "Name": "Author"
+        }, {
+            "Name": "SubjectTerms",
+            "CombineMode": "and",
+            "Count": "6"
+        }, {
+            "Name": "PublicationDate",
+            "Type": "Range",
+            "Label": "Publication Date"
+        }, {
+            "Name": "Genre"
+        }, {
+            "Label": "Region",
+            "Name": "GeographicLocations"
+        }, {
+            "Label": "Time Period",
+            "Name": "TemporalSubjectTerms"
+        }, {
+            "Name": "Language",
+            "Count": "6"
+        }],
+        "facets2": [{
+            "Name": "ContentType",
+            "Count": "6"
+        }, {
+            "Name": "Institution",
+            "Count": "6"
+        }, {
+            "Name": "Library",
+            "Count": "6"
+        }, {
+            "Name": "Discipline"
+        }, {
+            "Name": "Author"
+        }, {
+            "Name": "SubjectTerms",
+            "CombineMode": "and",
+            "Count": "6"
+        }, {
+            "Name": "PublicationDate",
+            "Type": "Range",
+            "Label": "Publication Date"
+        }, {
+            "Name": "Genre"
+        }, {
+            "Label": "Region",
+            "Name": "GeographicLocations"
+        }, {
+            "Label": "Time Period",
+            "Name": "TemporalSubjectTerms"
+        }, {
+            "Name": "Language",
+            "Count": "6"
+        }],
+        "quick_filters": true,
+        "whitelist_institution": "Catholic University of America (CU)",
+        "whitelist_library": "CU: Mullen Library Stacks"
+    },
+    "info": {
+        "imageRetrieverPriority": ["OpenLibrary", "LibraryThing", 'Google'],
+        "name": "cua",
+        "availabilityRateLimit": 0,
+        "libhash": "AG7FP7VA8V",
+        "dbs": [{
+            "dbids": ["W9Z", "Z6W"],
+            "availabilityLink": {
+                "url": "http://www.lib.cua.edu/summonrta/rtaxml.php?id=${id}&source=wrlc"
+            },
+            "availabilityResponseParser": "com.proquest.magnolia.api.availability.parsers.XPathAvailabilityResponseParser",
+            "availabilityParserProperties": {
+                "responseType": "XML",
+                "enableJavascript": true,
+                "itemListPath": "/",
+                "itemPath": "//item",
+                "locationPath": "callnumber//child::text()",
+                "statusPath": "status//child::text()",
+                "statusMessagePath": "status//child::text()",
+                "callNumberPath": "location//child::text()",
+                "availableRegex": ".*Available.*",
+                "notAvailableRegex": ".*Not Available.*"
+            }
+        }, {
+            "dbids": ["7RG"],
+            "availabilityLink": {
+                "url": "http://www.lib.cua.edu/summonrta/rtaxml.php?id=m${id}&source=georgemason"
+            },
+            "availabilityResponseParser": "com.proquest.magnolia.api.availability.parsers.XPathAvailabilityResponseParser",
+            "availabilityParserProperties": {
+                "responseType": "XML",
+                "enableJavascript": false,
+                "itemListPath": "/",
+                "itemPath": "//item",
+                "locationPath": "callnumber//child::text()",
+                "statusPath": "status//child::text()",
+                "statusMessagePath": "status//child::text()",
+                "callNumberPath": "location//child::text()",
+                "availableRegex": ".*Available.*",
+                "notAvailableRegex": ".*Not Available.*"
+            }
+        }, {
+            "dbids": ["-G0"],
+            "availabilityLink": {
+                "url": "http://www.lib.cua.edu/summonrta/rtaxml.php?id=${id}&source=georgetown"
+            },
+            "availabilityResponseParser": "com.proquest.magnolia.api.availability.parsers.XPathAvailabilityResponseParser",
+            "availabilityParserProperties": {
+                "responseType": "XML",
+                "enableJavascript": false,
+                "itemListPath": "/",
+                "itemPath": "//item",
+                "locationPath": "callnumber//child::text()",
+                "statusPath": "status//child::text()",
+                "statusMessagePath": "status//child::text()",
+                "callNumberPath": "location//child::text()",
+                "availableRegex": ".*Available.*",
+                "notAvailableRegex": ".*Not Available.*"
+            }
+        }],
+        "disableOneClick": false,
+        "linkingPriorities": {},
+        "participantRecordPrioritization": {
+            "preferredInstitutions": ["Catholic University of America (CU)"],
+            "preferredLibraries": ["CU: Mullen Library Stacks", "CU: Arch & Plan Library", "CU: Music Library", "CU: Central Reserve", "CU: Nursing/Biology Library Stacks", "CU: Physics Library", "CU: Physics Stacks", "CU: Electronic Book", "CU: Electronic Resource", "CU: Mullen Library Stacks Periodicals", "CU: Catholic University", "CU: Mullen Library Stacks Juvenile Fiction"]
+        },
+        "syndeticsClient": "",
+        "institutionName": "Catholic University of America (CU)",
+        "holdingsExcludeCOC": false,
+        "topics": {
+            "enabled": true
+        },
+        "authenticatedIpRanges": ["136.242.0.0/16", "192.245.136.3/32"],
+        "authenticated": true
+    },
+    "proxy": {
+        "_configVersion": 1,
+        "url": "http://proxycu.wrlc.org/login?url=",
+        "vendor": "EZProxy"
+    }
+}
